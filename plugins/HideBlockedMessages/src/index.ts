17a1552816ec9cc462e381af921b8197951dc5dd
MCH2024 <130590930+MCH2024@users.noreply.github.com>
2025-05-03 10:54:35 -0400

    Update index.ts

--- plugins/HideBlockedMessages/src/index.ts
+++ plugins/HideBlockedMessages/src/index.ts
@@ -1,136 +1,91 @@
-import { FluxDispatcher } from '@vendetta/metro/common';
-import { before, } from "@vendetta/patcher"
-import { findByProps, findByName } from "@vendetta/metro"
-import { logger } from "@vendetta"
+import { definePluginSettings, migratePluginSetting } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import { runtimeHashMessageKey } from "@utils/intlHash";
+import { Logger } from "@utils/Logger";
+import definePlugin, { OptionType } from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+import { i18n } from "@webpack/common";
+import { Message } from "discord-types/general";
 
-const RowManager = findByName("RowManager");
+const RelationshipStore = findByPropsLazy("getRelationships", "isBlocked");
 
-const RelationshipStore = findByProps("getRelationships", "isBlocked");
-const pluginName = "HideBlockedMessages";
-  
-function constructMessage(message, channel ) {
-    let msg = {
-        id: '',
-        type: 0,
-        content: '',
-        channel_id: channel.id,
-        author: {
-            id: '',
-            username: '',
-            avatar: '',
-            discriminator: '',
-            publicFlags: 0,
-            avatarDecoration: null,
-        },
-        attachments: [],
-        embeds: [],
-        mentions: [],
-        mention_roles: [],
-        pinned: false,
-        mention_everyone: false,
-        tts: false,
-        timestamp: '',
-        edited_timestamp: null,
-        flags: 0,
-        components: [],
-    };
-
-    if (typeof message === 'string') msg.content = message;
-    else msg = { ...msg, ...message };
-
-    return msg;
-};
-
-// Function to check blocked users
-const isBlocked = (id) => {
-    return RelationshipStore.isBlocked(id);
-};
-
-let patches = [];
-
-const startPlugin = () => {
-    try {
-        // Main patch
-        const patch1 = (
-            before("dispatch", FluxDispatcher, ([event]) => {
-                // Hides blocked messages on channel loads
-                if (event.type === "LOAD_MESSAGES_SUCCESS") {
-                    event.messages = event.messages.filter((message) => { 
-                        return (!isBlocked(message?.author?.id));
-                    });
-                }
-                // Hides blocked messages on message creation/update
-                if (event.type === "MESSAGE_CREATE" || event.type === "MESSAGE_UPDATE") {
-                    let message = event.message;
-
-                    if (isBlocked(message?.author?.id)) {
-                        // Drop event
-                        event.channelId = "0"
-                    };
-                }
-            })
-        );
-        patches.push(patch1);
-
-        // Fallback patch to mostly remove blocked message rows if main patch doesn't work on first load
-        const patch2 = (
-            before("generate", RowManager.prototype, ([data]) => {
-                if (isBlocked(data.message?.author?.id)) {
-                    data.renderContentOnly = true
-                    data.message.content = null
-                    data.message.reactions = []
-                    data.message.canShowComponents= false
-                    if (data.rowType === 2) {
-                        data.roleStyle = ""
-                        data.text = "[Temp] Blocked message. Reloading should fix."
-                        data.revealed = false
-                        data.content = []
-                    }
-                }
-            })
-        );
-        patches.push(patch2);
-
-        logger.log(`${pluginName} loaded.`);
-        return null;
-    } catch (err) {
-        logger.log(`[${pluginName} Error]`, err);
-    };
+interface MessageDeleteProps {
+    // Internal intl message for BLOCKED_MESSAGE_COUNT
+    collapsedReason: () => any;
 }
 
-// Load Plugin
-const onLoad = () => {
-    logger.log(`Loading ${pluginName}...`);
-
-    // Dispatch with a fake event to enable the action handlers from first loadup
-    for (let type of ["MESSAGE_CREATE", "MESSAGE_UPDATE"]) {
-        logger.log(`Dispatching ${type} to enable action handler.`);
-        FluxDispatcher.dispatch({
-            type: type,
-            message: constructMessage('PLACEHOLDER', { id: '0' }),
-        });
-    };
-
-    // Dispatch with a fake event to enable the action handlers from first loadup
-    for (let type of ["LOAD_MESSAGES", "LOAD_MESSAGES_SUCCESS"]) {
-        logger.log(`Dispatching ${type} to enable action handler.`);
-        FluxDispatcher.dispatch({
-            type: type,
-            messages: [],
-        });
-    };
-
-    // Begin patch sequence
-    startPlugin();
-};
-
-export default {
-    onLoad,
-    onUnload: () => {
-        logger.log(`Unloading ${pluginName}...`);
-        for (let unpatch of patches) {
-            unpatch();
-        };
-        logger.log(`${pluginName} unloaded.`);
+// Remove this migration once enough time has passed
+migratePluginSetting("NoBlockedMessages", "ignoreBlockedMessages", "ignoreMessages");
+const settings = definePluginSettings({
+    ignoreMessages: {
+        description: "Completely ignores incoming messages from blocked and ignored (if enabled) users",
+        type: OptionType.BOOLEAN,
+        default: false,
+        restartNeeded: true
+    },
+    applyToIgnoredUsers: {
+        description: "Additionally apply to 'ignored' users",
+        type: OptionType.BOOLEAN,
+        default: true,
+        restartNeeded: false
     }
-};
+});
+
+export default definePlugin({
+    name: "NoBlockedMessages",
+    description: "Hides all blocked/ignored messages from chat completely",
+    authors: [Devs.rushii, Devs.Samu, Devs.jamesbt365],
+    settings,
+
+    patches: [
+        {
+            find: ".__invalid_blocked,",
+            replacement: [
+                {
+                    match: /let{expanded:\i,[^}]*?collapsedReason[^}]*}/,
+                    replace: "if($self.shouldHide(arguments[0]))return null;$&"
+                }
+            ]
+        },
+        ...[
+            '"MessageStore"',
+            '"ReadStateStore"'
+        ].map(find => ({
+            find,
+            predicate: () => settings.store.ignoreMessages,
+            replacement: [
+                {
+                    match: /(?<=function (\i)\((\i)\){)(?=.*MESSAGE_CREATE:\1)/,
+                    replace: (_, _funcName, props) => `if($self.shouldIgnoreMessage(${props}.message))return;`
+                }
+            ]
+        }))
+    ],
+
+    shouldIgnoreMessage(message: Message) {
+        try {
+            if (RelationshipStore.isBlocked(message.author.id)) {
+                return true;
+            }
+            return settings.store.applyToIgnoredUsers && RelationshipStore.isIgnored(message.author.id);
+        } catch (e) {
+            new Logger("NoBlockedMessages").error("Failed to check if user is blocked or ignored:", e);
+            return false;
+        }
+    },
+
+    shouldHide(props: MessageDeleteProps): boolean {
+        try {
+            const collapsedReason = props.collapsedReason();
+            const blockedReason = i18n.t[runtimeHashMessageKey("BLOCKED_MESSAGE_COUNT")]();
+            const ignoredReason = settings.store.applyToIgnoredUsers
+                ? i18n.t[runtimeHashMessageKey("IGNORED_MESSAGE_COUNT")]()
+                : null;
+
+            return collapsedReason === blockedReason || collapsedReason === ignoredReason;
+        } catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
+});
